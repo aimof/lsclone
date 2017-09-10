84ff03a7fb27a3a20f70b12784daf73c59324e7f
aimof <aimof.aimof@gmail.com>
2017-09-10 20:08:44 +0900

    Simple version
    It prints all files and directories in current directory

--- main.go
+++ main.go
@@ -1,3 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+func main() {
+	targetPath, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	isDir, err := isDirectory(targetPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if !isDir {
+		log.Fatalln(err)
+	}
+	files, err := ioutil.ReadDir(targetPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, file := range files {
+		fmt.Printf(file.Name() + "  ")
+	}
+	fmt.Printf("\n")
+}
+
+func isDirectory(path string) (isDir bool, err error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return info.IsDir(), nil
+}
